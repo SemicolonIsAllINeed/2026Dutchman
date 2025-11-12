5c53d80db5ed4b54ce03783d4b50c4fdf21a6568
Sammy <sammy.bikki@gmail.com>
2025-11-12 11:32:43 -0500

    W Progress

--- src/main/java/frc/robot/commands/autons/LimelightTest.java
+++ src/main/java/frc/robot/commands/autons/LimelightTest.java
@@ -1,17 +1,24 @@
 package frc.robot.commands.autons;
 
+import static frc.robot.configs.constants.PhysicalConstants.Elevator.ELEVATOR_L1_HEIGHT;
+import static frc.robot.configs.constants.PhysicalConstants.Elevator.ELEVATOR_L2_HEIGHT;
+
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.commands.Elevate;
+import frc.robot.configs.constants.PhysicalConstants.Elevator;
 import frc.robot.commands.GoToAprilTag;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
 import frc.robot.subsystems.Vision;
 
+//This is a test to check if Go To April Tag will work for pathplanner
 public class LimelightTest extends SequentialCommandGroup {
     public LimelightTest(CommandSwerveDrivetrain drivetrain, Vision vision, int tagID) {
         addCommands(
             new SequentialCommandGroup(
-                new WaitCommand(1),
-                new GoToAprilTag(drivetrain, vision, tagID).withTimeout(5.0)
+                new WaitCommand(0.2),
+                new GoToAprilTag(drivetrain, vision, tagID).withTimeout(5.0),
+                new Elevate(ELEVATOR_L2_HEIGHT)
                 )
         );
     }
